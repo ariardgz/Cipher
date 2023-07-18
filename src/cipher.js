b6b814c42973c729ba2b70cdc906cd2f2e3fdc21
Ariadna Rodriguez <ariadna.rodriguez30@hotmail.com>
2023-07-18 17:00:03 -0600

    Primera prueba JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,15 @@
 const cipher = {
-  // ...
+  word: "",
+  encode: function () {
+  
+    var num = this.word.charCodeAt(0);
+    console.log(num);
+
+
+
+  },
+   
+  
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import cipher from './cipher.js';
+window.addEventListener("load", inicio, true);
 
-console.log(cipher);
+//const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+//var inputDecodificado = document.getElementById("message").value;
+//var inputCodificado = document.getElementById("message2");
+
+function inicio(){
+    document.getElementById("message").addEventListener("keyup", function(){
+        this.value = this.value.toUpperCase();
+    
+}, true);
+}
+function Encode(inputCodificado){
+    
+    
+    var solved ="";
+    for( var i= 0; i<inputCodificado.length; i++){
+        var asciNum = inputCodificado.charCodeAt();
+        if (asciNum >=65 && asciNum <= 77){
+            solved += String.fromCharCode(asciNum + 3);
+
+        }else{
+        solved +=inputCodificado[i];
+        
+    } 
+
+    }
+    return solved;
+    
+    
+}
+
+function Decode(Decode){
+    var solved ="";
+    for (var i= 0; i<inputDecodificado.length; i++){
+        var asciNum = inputDecodificado.charCodeAt();
+        if (asciNum >= 78 && asciNum <=90){
+            solved += String.fromCharCode(asciNum-3);
+        }else{
+            solved += Decode[i];
+        }
+    }
+
+}
